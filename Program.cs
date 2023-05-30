73a52313d49cef0167b58010690ced9d9388d833
juliarcoelho <juliarochx@gmail.com>
2023-05-30 19:44:47 -0300

    commitando arquivos iniciais do projeto

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace imc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, situacao = "";
+            double peso, altura, imc;
+            string continua = "n";
+
+            for (int x = 0; x < 5; x++){
+                Console.WriteLine(x);
+            }
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("Digite o seu nome:");
+                nome = Console.ReadLine();
+
+                Console.WriteLine("Digite o seu peso:");
+                peso = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite o seu altura:");
+                altura = Convert.ToDouble(Console.ReadLine());
+
+                Console.Clear();
+
+                imc = peso / (altura * altura);
+
+                Console.WriteLine(nome + " Seu IMC é " + imc);
+
+                if (imc <= 20)
+                {
+                    situacao = " abaixo do peso.";
+                }
+                else
+                {
+                    if (imc > 20 && imc < 25)
+                    {
+                        situacao = " peso normal.";
+                    }
+                    else
+                    {
+                        if (imc >= 25)
+                        {
+                            situacao = " acima do peso";
+                        }
+                    }
+                }
+                Console.WriteLine(nome + " a sua situação é " + situacao);
+                Console.WriteLine("Quer continuar? (s/n)");
+                continua = Console.ReadLine();
+
+            } while (continua == "s");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Calculadora de IMC (Índice de Massa Corporal) em C#
+
+Este é um programa em C#, feito para meu curso técnico no Senac, que solicita dados do usuário, como nome, peso e altura, calcula o IMC e retorna se a pessoa está abaixo, dentro ou acima do peso. O programa utiliza estruturas de repetição, como `while` e `for`.
+
+## Funcionalidades
+
+- Solicita ao usuário que insira seus dados pessoais, incluindo nome, peso e altura.
+- Calcula o IMC com base no peso e altura fornecidos.
+- Classifica o estado de peso do usuário com base no IMC calculado, retornando se a pessoa está abaixo, dentro ou acima do peso.
+
+## Pré-requisitos
+
+- Ambiente de desenvolvimento compatível com C# 
+- .NET Framework (ou versão superior)
+
+
